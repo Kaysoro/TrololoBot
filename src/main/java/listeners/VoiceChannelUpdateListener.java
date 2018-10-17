36925c99839619ca2a10edf8f5442385e2eb123f
Steven Pescheteau <steven.pescheteau@gmail.com>
2018-10-17 03:08:46 +0200

    Voice channel update managed (bug corrected)

--- src/main/java/listeners/VoiceChannelUpdateListener.java
+++ src/main/java/listeners/VoiceChannelUpdateListener.java
@@ -4,9 +4,8 @@ import controllers.NotificationControl;
 import data.DiscordRegistry;
 import javafx.application.Platform;
 import sx.blah.discord.api.events.EventSubscriber;
-import sx.blah.discord.handle.impl.events.guild.channel.ChannelCategoryUpdateEvent;
-import sx.blah.discord.handle.impl.events.guild.channel.ChannelUpdateEvent;
-import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.impl.events.guild.voice.VoiceChannelUpdateEvent;
+import sx.blah.discord.handle.obj.IVoiceChannel;
 
 /**
  * Created by kaysoro on 07/10/2018.
@@ -14,11 +13,10 @@ import sx.blah.discord.handle.obj.IChannel;
 public class VoiceChannelUpdateListener {
 
     @EventSubscriber
-    public void onReady(ChannelUpdateEvent event) {
-        if (!(event instanceof ChannelCategoryUpdateEvent)) {
-            DiscordRegistry.get(IChannel.class, event.getNewChannel().getLongID()).checkIntegrity();
-            Platform.runLater(() -> NotificationControl.updateEvent("Voice channel updated: "
-                    + event.getGuild().getName() + " > " + event.getNewChannel().getName()));
-        }
+    public void onReady(VoiceChannelUpdateEvent event) {
+        DiscordRegistry.get(IVoiceChannel.class, event.getNewVoiceChannel().getLongID()).checkIntegrity();
+        Platform.runLater(() -> NotificationControl.updateEvent("Voice channel updated: "
+                + event.getGuild().getName() + " > " + event.getNewVoiceChannel().getName()));
+
     }
 }
